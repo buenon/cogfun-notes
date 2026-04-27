7c7360c4e1aeb7f7968cf2323a98234662f586a8
Nadav Bueno <buenonadav@gmail.com>
2026-04-27 19:06:10 +0300

    Fix: add basename to BrowserRouter for subpath support

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,9 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
+
   </StrictMode>,
 );
